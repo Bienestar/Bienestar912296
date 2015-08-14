8ce8f9eec410008bf181d6d1ffc47dc432673996
ErikaJu <erikacontrerasc@misena.edu.co>
2015-08-14 08:24:05 -0500

    Proyecto Bienestar

---
+++ database/migrations/2015_08_14_131758_create_acudientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAcudientesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('acudientes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string ('Nombre_Acudiente', 20);
+			$table->string ('Apellido_Acudiene', 30);
+			$table->bigInteger ('Teleofono_Acudiene';
+			$table->string ('Direccion_Acudiente', 60);
+			$table->bigInteger('Ncelular_Acudineee')
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('acudientes');
+	}
+
+}
