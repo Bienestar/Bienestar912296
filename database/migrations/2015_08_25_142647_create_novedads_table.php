60d872d8b10d92059c92672e8cfa72b78a62b179
WilliamCQZ413 <William-0402@hotmail.com>
2015-08-26 21:44:38 -0500

    Proyecto V.1

---
+++ database/migrations/2015_08_25_142647_create_novedads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNovedadsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('novedads', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('Tipo_Novedad', 30);
+			$table->string('Descripcion',300);
+			
+
+			//asignamos nuevo atributo para llave foranea
+			$table->integer('Fk_IdAsesor')->unsigned();
+
+			//creamos la relacion
+			$table->Foreign('Fk_IdAsesor')->references('id')->on('Asesors');
+			$table->timestamps();
+			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('novedads');
+	}
+
+}
